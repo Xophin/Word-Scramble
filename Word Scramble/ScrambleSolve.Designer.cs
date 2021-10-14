0be0f2930465cb4579b02240ab2a29df7bbb9dab
Xophin <stripedmango@hotmail.com>
2021-10-14 14:23:35 -0400

    Add project files.

---
+++ Word Scramble/ScrambleSolve.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace Word_Scramble
+{
+    partial class frmScrambleSolve
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.txtAnswer = new System.Windows.Forms.TextBox();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblScrambledWord = new System.Windows.Forms.Label();
+            this.btnStartScramble = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(99, 63);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(73, 38);
+            this.btnCheck.TabIndex = 0;
+            this.btnCheck.Text = "Check Answer";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // txtAnswer
+            // 
+            this.txtAnswer.Location = new System.Drawing.Point(60, 37);
+            this.txtAnswer.Name = "txtAnswer";
+            this.txtAnswer.Size = new System.Drawing.Size(181, 20);
+            this.txtAnswer.TabIndex = 1;
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Location = new System.Drawing.Point(12, 40);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(45, 13);
+            this.lblAnswer.TabIndex = 2;
+            this.lblAnswer.Text = "Answer:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Word to Solve:";
+            // 
+            // lblScrambledWord
+            // 
+            this.lblScrambledWord.AutoSize = true;
+            this.lblScrambledWord.Location = new System.Drawing.Point(96, 9);
+            this.lblScrambledWord.Name = "lblScrambledWord";
+            this.lblScrambledWord.Size = new System.Drawing.Size(0, 13);
+            this.lblScrambledWord.TabIndex = 4;
+            // 
+            // btnStartScramble
+            // 
+            this.btnStartScramble.Location = new System.Drawing.Point(15, 63);
+            this.btnStartScramble.Name = "btnStartScramble";
+            this.btnStartScramble.Size = new System.Drawing.Size(73, 38);
+            this.btnStartScramble.TabIndex = 5;
+            this.btnStartScramble.Text = "Get Scramble";
+            this.btnStartScramble.UseVisualStyleBackColor = true;
+            this.btnStartScramble.Click += new System.EventHandler(this.btnStartScramble_Click);
+            // 
+            // frmScrambleSolve
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(305, 114);
+            this.Controls.Add(this.btnStartScramble);
+            this.Controls.Add(this.lblScrambledWord);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.txtAnswer);
+            this.Controls.Add(this.btnCheck);
+            this.Name = "frmScrambleSolve";
+            this.Text = "Scramble Solve";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.TextBox txtAnswer;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblScrambledWord;
+        private System.Windows.Forms.Button btnStartScramble;
+    }
+}
+
---
+++ Word Scramble/ScrambleSolve.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Word_Scramble
+{
+    public partial class frmScrambleSolve : Form
+    {
+        // Create the initial List of words.
+        List<String> lstDefaultList = new List<String>() { "Moon", "Soon", "Loom", "Craft", "Mine", "Town", "Cute", "Cozy", "Quail", "Quilt", "Lost", "Shoe" };
+        Random random = new Random();
+        String strOriginalWord = "";
+
+        public frmScrambleSolve()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnStartScramble_Click(object sender, EventArgs e)
+        {
+            // This will randomly pick a word from the word list then send it through the scrambler.
+            String strScrambledWord = "";
+            int intWordListLength = 0;
+            int intSelectedWordIndex = 0;
+
+            // Check how long the list is.
+            for (int i = 0; i < lstDefaultList.Count; i++)
+            {
+                intWordListLength = i;
+            }
+            
+            if(intWordListLength <= 0)
+            {
+                MessageBox.Show("There are no words in the word list. Please add words to the word list before trying to scramble.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                // Pick a random word from the List
+                intSelectedWordIndex = random.Next(-1, intWordListLength);
+
+                strOriginalWord = lstDefaultList[intSelectedWordIndex];
+
+                // Check string length
+                if(strOriginalWord.Length > 0)
+                {
+                    // Get scrambled version of word
+                    strScrambledWord = Scrambler(strOriginalWord, strOriginalWord.Length);
+
+                    // Change the label text for the scrambled word to the scrambled word.
+                    lblScrambledWord.Text = strScrambledWord;
+
+                }
+                else
+                {
+                    MessageBox.Show("The word selected had no characters.  Word selected was at index " + intSelectedWordIndex.ToString() + " of list lstWordsToScramble", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+            }
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            if(txtAnswer.Text == strOriginalWord)
+            {
+                MessageBox.Show("Success! You unscrambled the word correctly.", "Success", MessageBoxButtons.OK);
+                lblScrambledWord.Text = "";
+                txtAnswer.Text = "";
+                // add counter for correct answers here.
+                // Maybe something for tries?
+                // Would need class level variable.
+            }
+            else
+            {
+                MessageBox.Show("Incorrect. That is not the word. Please try again.", "Incorrect", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private String Scrambler(String strOriginalWord, int intWordLength)
+        {
+            // Takes in a word, uses Random class to shuffle the word, then passes the shuffled word out.
+            // Code comes from a comment by user nan on stackoverflow with minor edit.
+            // edits include: changing a var out for a char. 
+            char chrSwap = ' ';
+            char[] chrScramble;
+            int intFirstIndex = 0;
+            int intCharacterCount = intWordLength;
+            int intScrambleIndex = 0;
+            int intShuffleCount = 200;
+
+            // Converts string to character array for easier manipulation.
+            chrScramble = strOriginalWord.ToCharArray(0, intWordLength);
+
+            // Shuffle the word multiple times.
+            while(intShuffleCount > 1)
+            {
+                intShuffleCount--;
+                intCharacterCount = intWordLength;
+                while (intCharacterCount > 1)
+                {
+                    // We move backwards through the original word.
+                    intCharacterCount--;
+                    // grabbing the index of the first character to shuffle.
+                    intScrambleIndex = random.Next(intCharacterCount + 1);
+                    // grabbing the first character to shuffle
+                    chrSwap = chrScramble[intScrambleIndex];
+                    // Shuffle! We replace the character that we saved off into chrSwap with the one from from where chrSwap will go
+                    chrScramble[intScrambleIndex] = chrScramble[intCharacterCount];
+                    // putting chrSwap back into the array.
+                    chrScramble[intCharacterCount] = chrSwap;
+                }
+            }
+            
+            // Return the Scrambled word as a String.
+            String strScrambledWord = new string(chrScramble);
+            return strScrambledWord;
+        }
+    }
+}
